ee8b9e5f7e8764b63834b81179c816d5402adc89
João Pedro <101525753+JoaoPedroDevOz@users.noreply.github.com>
2026-06-03 20:59:40 -0300

    Refactor data processing and analysis in descricaoDF.py
    Refactored the code to include data cleaning and analysis functions, improving readability and maintainability.

--- descricaoDF.py
+++ descricaoDF.py
@@ -4,54 +4,67 @@ import pandas as pd
 
 df = pd.read_csv('dados_violencia_mulheres_ses_2026.csv', encoding='utf-8', sep=';')
 
+dfAnalytic = pd.DataFrame(df);
+
+#================================
+#-------FUNÇÕES AUXILIARES-------
+#================================
+
+def convertToDateTime(df, seriesColumn):
+    df[seriesColumn] = pd.to_datetime(df[seriesColumn])
+
+
+def findNulls(df, columName=''):
+    if (columName):
+        return df[columName].isnull().sum()
+    return df.isnull().sum()
+
 #================================
 #------ANALISE EXPLORATÓRIA------
 #================================
 
-# print(df.info()) # todas as colunas contém valores não nulos
-
-## Dependendo da IDE utilizada para compilar o projeto, os próximos comandos não são necessários
-
-## Verificando as dimensões do DataFrame
-
-# linhas, colunas = df.shape
-# print(f'Linhas: {linhas}, Colunas: {colunas}')
-
-# print(df.head()) # verificação dos tipos dos dados
-
-
-## Descreve de forma estatística dados numéricos e quantifica valores em string ou booleano
-
-# print(df.describe())
-
+def describeDF(df):
+    print(df.info()) # todas as colunas contém valores não nulos
+    
+    ## Dependendo da IDE utilizada para compilar o projeto, os próximos comandos não são necessários
+    
+    ## Verificando as dimensões do DataFrame
+    
+    # linhas, colunas = df.shape
+    # print(f'Linhas: {linhas}, Colunas: {colunas}')
+    
+    print(df.head()) # verificação dos tipos dos dados
+    
+    
+    ## Descreve de forma estatística dados numéricos e quantifica valores em string ou booleano
+    
+    print(df.describe())
+    
+    
 #===================================
 #---LIMPEZA E TRATAMENTO DE DADOS---
 #===================================
+
+def populateBySubDates(dateInit, dateEnd):
+    return 
 
 ## A limpeza e tratamento dos dados deve ser feita de forma minunciosa, como valores 'nan' ou 'null' na tabela para 
 ## analisar os dados de forma efetiva sem considerar ocorrências cujos valores não fomentam a investigação e estudo dos dados.
 
 ## Verificando valores nulos
-
-# print(df.isnull().sum())
+print(f'Colunas com valores nulos: \n{findNulls(df)}\n')
 
 ### primeira análise: há 72 campos da coluna dt_nasc que estão nulas e apenas 3 em nu_idade_n
     #### dt_nasc        72
     #### nu_idade_n      3
 
-# print(df[(df['dt_nasc'].isnull()) & (df['nu_idade_n'].isnull())]) # As três linhas com nu_idade_n nulos estão com dt_nasc nulos também, pode-se desconsiderar essas linhas por conter os dados "faltoso", assim sendo deletadas.
-
 df = df.dropna(subset=['nu_idade_n'])
 
-# print(df['nu_idade_n'].isnull().sum()) # Resp: 0
+print(f"A coluna dt_nasc possui: {findNulls(df, 'dt_nasc')} valores nulos")
 
-print(df['dt_nasc'].isnull().sum())
-
-## Segunda parte é popular a coluna dt_nasc a partir do nu_idade_n, fazendo o cálculo do ano subtraido pelo valor
+## Segunda parte é popular a coluna dt_nasc a partir da subtração entre a coluna nu_idade com a dt_notific.
 ## Isso gera valores artificiais, não reais, mas é importante para a estimativa que será analisada.
 
-# data hoje
-data_hoje = pd.Timestamp.today()
 
 print(df.loc[df['nu_idade_n'].astype(str).str.match(r'^7\.'), :]) # não há valores com zeros à frente.
 
@@ -68,9 +81,13 @@ df['nu_idade_n'] = (
 df['nu_idade_n'] = pd.to_numeric(df['nu_idade_n'], errors='coerce')
 
 
+#===================================
+#-----ESTUDO E ANÁLISE DE DADOS-----
+#===================================
 
 # Garantir que a coluna é datetime
-df["dt_nasc"] = pd.to_datetime(df["dt_nasc"])
+
+convertToDateTime(df, "dt_nasc")
 
 # Dividindo em partes o estudo
 
@@ -94,19 +111,22 @@ df["dt_nasc"] = pd.to_datetime(df["dt_nasc"])
     #### 'viol_sexu' para identificar se é violência sexual
 
     
-## Uma informação relevante é identificar em quais meses esses casos tiveram picos.
-
-# Garantir que a coluna é datetime
-df["dt_notific"] = pd.to_datetime(df["dt_notific"])
-
-df_dt_notific = (
-    df.groupby([
-        df["dt_notific"].dt.year.rename("ano"),
-        df["dt_notific"].dt.month.rename("mes")
-    ])
-    .size()
-    .reset_index(name="quantidade")
-)
+def sepByDateTime():
+    ## Uma informação relevante é identificar em quais meses esses casos tiveram picos.
+    
+    # Garantir que a coluna é datetime
+    dfAnalytic["dt_notific"] = pd.to_datetime(df["dt_notific"])
+    
+    return (
+        df.groupby([
+            dfAnalytic["dt_notific"].dt.year.rename("ano"),
+            dfAnalytic["dt_notific"].dt.month.rename("mes")
+        ])
+        .size()
+        .reset_index(name="quantidade")
+    )
+    
+df_dt_notific = sepByDateTime()
 
 # Divisão das gerações para estudo por idades
 
